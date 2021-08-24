657da90e5d4105913c26e1421870f1bb3429663a
miraz <www.miraz1234567@gmail.com>
2021-08-24 11:54:46 +0600

    modified

--- Leetcode/Top_100_Liked_Questions/279_Perfect_Square.java
+++ Leetcode/Top_100_Liked_Questions/279_Perfect_Square.java
@@ -12,3 +12,23 @@ class Solution {
         return dp[n];
     }
 }
+
+//Lagranges 4 square theorem and Legendries 3 square therem
+class Solution {
+    public int numSquares(int n) {
+        if(isPerfectSquare(n))return 1;
+        for(int i = 1; i * i <= n; i++){
+            // separate into two perfect square if it has any
+            if(isPerfectSquare(n-i*i))return 2;
+        }
+        
+        while(n%4 == 0) n /= 4;
+        if(n %8 == 7)return 4;//if the number is 4^a(4b+7) form
+        return 3;
+    }
+    
+    public boolean isPerfectSquare(int n){
+        int sqrt = (int)Math.sqrt(n);
+        return sqrt*sqrt == n;
+    }
+}
