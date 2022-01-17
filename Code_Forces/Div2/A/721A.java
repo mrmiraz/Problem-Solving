0ffeb5c8e965b245889903c6cfa51e1b44a9af01
Mahmdudur Rahman Miraz <84429027+mrmiraz@users.noreply.github.com>
2022-01-17 17:43:25 +0600

    One-dimensional Japanese Crossword

---
+++ Code_Forces/Div2/A/721A.java
@@ -0,0 +1,29 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String s = sc.next();
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int key = 0;
+        if(s.charAt(0) == 'B')
+            map.put(key, 1);
+        for(int i = 1; i < n; i++){
+            if(s.charAt(i) == 'B'){
+                if(s.charAt(i-1) == 'W'){
+                    map.put(++key, 1);
+                }
+                else
+                    map.put(key, map.get(key)+1);
+            }     
+        }
+        System.out.println(map.size());
+        for(int i : map.values()){
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+}
