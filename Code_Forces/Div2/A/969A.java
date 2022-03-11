4e384987e2283b24e39d6e0d65f8bc7a3fca152a
Mahmdudur Rahman Miraz <84429027+mrmiraz@users.noreply.github.com>
2022-03-11 23:13:59 +0600

    Mahmoud and Ehab and the even-odd game

---
+++ Code_Forces/Div2/A/969A.java
@@ -0,0 +1,12 @@
+
+import java.util.*;
+public class Solution{  
+  public static void main(String[] args){  
+    Scanner sc = new Scanner(System.in) ;
+    int n= sc.nextInt();
+    if(n%2 ==0)
+        System.out.println("Mahmoud");
+    else
+       System.out.println("Ehab");
+  }  
+}  
