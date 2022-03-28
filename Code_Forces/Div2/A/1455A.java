d06b2b5810e06a1a5fbe173a1ea39fa37cec8f3a
Mahmdudur Rahman Miraz <84429027+mrmiraz@users.noreply.github.com>
2022-03-28 16:38:38 +0700

    Strange Functions

---
+++ Code_Forces/Div2/A/1455A.java
@@ -0,0 +1,21 @@
+
+
+import java.util.*;
+
+
+public class Main{
+	static Scanner sc = new Scanner(System.in);
+
+	public static void solver(){
+		String s = sc.next();
+		System.out.println(s.length());
+	}
+
+	public static void main(String[] args){
+		int t = sc.nextInt();
+		while(t-->0){
+			solver();
+		}
+		
+	}
+}
