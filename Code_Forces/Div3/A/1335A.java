82657c4a34b974ef53e71bc564a19291866996b7
Mahmdudur Rahman Miraz <84429027+mrmiraz@users.noreply.github.com>
2022-04-01 10:39:18 +0600

    Candies and Two Sisters

---
+++ Code_Forces/Div3/A/1335A.java
@@ -0,0 +1,25 @@
+
+import java.util.*;
+
+
+public class Main{
+	static Scanner sc = new Scanner(System.in);
+
+	public static void solver(){
+		int n = sc.nextInt();
+
+		System.out.println((n-1)/2);
+		
+
+	}
+
+	public static void main(String[] args){
+		int t = sc.nextInt();
+
+		while(t-->0){
+			solver();	
+		}
+		
+		
+	}
+}
