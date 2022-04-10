359106e21354de5736a4eff5478d07fe0d931d45
Mahmdudur Rahman Miraz <84429027+mrmiraz@users.noreply.github.com>
2022-04-10 22:38:40 +0600

    Marriage Media

---
+++ LightOJ/LOJ-1184.java
@@ -0,0 +1,83 @@
+import java.util.*;
+public class Main {
+
+    static public class boys{
+        int bh, ba, bd;
+        boys(int bh, int ba, int bd){
+            this.bh = bh;
+            this.ba = ba;
+            this.bd = bd;
+        }
+    }
+    static Scanner sc = new Scanner(System.in);
+    
+    static ArrayList<Integer> graph[];
+    static boolean used[];
+    static int matchingArray[];
+    static int X, Y, edges;
+    
+    static boolean kuhn(int v){
+        if(used[v])return false;
+        used[v] = true;
+        for(int cNode: graph[v]){
+            if(matchingArray[cNode] == -1 || kuhn(matchingArray[cNode])){
+                matchingArray[cNode] = v;
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    @SuppressWarnings({"unchecked"})
+    static int solver(){
+        
+        
+        X = sc.nextInt(); Y = sc.nextInt();
+        used = new boolean[X]; 
+        graph = new ArrayList[X];
+        
+        for(int i = 0; i < X; i++){
+            graph[i] = new ArrayList<>();
+        }
+        
+        boys boy[] = new boys[X];
+        for(int i =0; i < X; i++){
+            int bh = sc.nextInt(), ba = sc.nextInt(), bd = sc.nextInt();
+            boy[i] = new boys(bh, ba, bd);
+        }
+
+        matchingArray = new int[Y];
+        Arrays.fill(matchingArray, -1);
+        
+        for(int i =0; i < Y; i++){
+            int gh = sc.nextInt(), ga = sc.nextInt(), gd = sc.nextInt();
+            for(int j = 0; j < X; j++){
+                int dh = Math.abs(boy[j].bh - gh);
+                int da = Math.abs(boy[j].ba - ga);
+                int dd = boy[j].bd - gd;
+                if(dh <= 12 && da <= 5 && dd == 0){
+                    graph[j].add(i);
+                }
+            }
+        }
+
+        int cnt = 0;
+        for(int i = 0; i < X; i++){
+            Arrays.fill(used, false);
+            if(kuhn(i)){
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+    
+    public static void main(String[] args) {
+        
+        int t = sc.nextInt();
+        int i =0;
+        while(t-->0){
+            int r = solver();
+            System.out.println("Case "+(++i) + ": "+ r);
+        }
+    }
+}
