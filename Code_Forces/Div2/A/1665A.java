9726e0416a76606aa4c4e4daee3e974a5b9dd48d
Mahmdudur Rahman Miraz <84429027+mrmiraz@users.noreply.github.com>
2022-04-11 14:18:24 +0600

    GCD vs LCM

---
+++ Code_Forces/Div2/A/1665A.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Solution {
+    static Scanner sc = new Scanner(System.in);
+    
+    public static void solver() {
+        int n = sc.nextInt();
+        System.out.println((n-3)+  " 1 1 1");
+    }
+    
+    public static void main(String[] args) {
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            solver();
+        }
+    }
+}
