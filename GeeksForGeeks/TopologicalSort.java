d2a4264abb897cbe04a55fb1abd9429ecfc54042
Mahmdudur Rahman Miraz <84429027+mrmiraz@users.noreply.github.com>
2022-05-23 23:31:35 +0600

    Topological Sort

---
+++ GeeksForGeeks/TopologicalSort.java
@@ -0,0 +1,95 @@
+/*
+Problem: Given a Directed Acyclic Graph (DAG) with V vertices and E edges, Find any Topological Sorting of that Graph.
+*/
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+
+        while (t-- > 0) {
+            ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+            String st[] = read.readLine().trim().split("\\s+");
+            int edg = Integer.parseInt(st[0]);
+            int nov = Integer.parseInt(st[1]);
+
+            for (int i = 0; i < nov + 1; i++)
+                list.add(i, new ArrayList<Integer>());
+
+            int p = 0;
+            for (int i = 1; i <= edg; i++) {
+                String s[] = read.readLine().trim().split("\\s+");
+                int u = Integer.parseInt(s[0]);
+                int v = Integer.parseInt(s[1]);
+                list.get(u).add(v);
+            }
+
+            int[] res = new Solution().topoSort(nov, list);
+
+            if (check(list, nov, res) == true)
+                System.out.println("1");
+            else
+                System.out.println("0");
+        }
+    }
+    static boolean check(ArrayList<ArrayList<Integer>> list, int V, int[] res) {
+        
+        if(V!=res.length)
+        return false;
+        
+        int[] map = new int[V];
+        for (int i = 0; i < V; i++) {
+            map[res[i]] = i;
+        }
+        for (int i = 0; i < V; i++) {
+            for (int v : list.get(i)) {
+                if (map[i] > map[v]) return false;
+            }
+        }
+        return true;
+    }
+}
+// } Driver Code Ends
+
+
+/*Complete the function below*/
+
+
+class Solution
+{
+    //Function to return list containing vertices in Topological order. 
+    static boolean[] visited;
+   
+    static Stack<Integer> stack;
+    static void dfs(int v,ArrayList<ArrayList<Integer>> adj){
+        visited[v] = true;
+        for(int child: adj.get(v)){
+           if(!visited[child]) {
+               dfs(child, adj);
+           }
+        }
+        stack.push(v);
+    }
+    
+    static int[] topoSort(int V, ArrayList<ArrayList<Integer>> adj) 
+    {
+        visited = new boolean[V];
+        stack = new Stack<>();
+        for(int i =0;  i < V; i++){
+            if(!visited[i]){
+                dfs(i, adj);
+            }
+        }
+        
+        int[] result = new int[V];
+        for(int i =0; i < V; i++){
+            result[i] = stack.pop();
+        }
+        return result;
+    }
+}
