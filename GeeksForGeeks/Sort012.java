f2c1629009975ef1a3c6ee4b9ff774a8fb7285dc
Mahmdudur Rahman Miraz <84429027+mrmiraz@users.noreply.github.com>
2022-06-15 21:35:48 +0600

    Sort an array of 0s, 1s and 2s

---
+++ GeeksForGeeks/Sort012.java
@@ -0,0 +1,71 @@
+/*
+problem:
+Given an array of size N containing only 0s, 1s, and 2s; sort the array in ascending order.
+
+
+Example 1:
+
+Input: 
+N = 5
+arr[]= {0 2 1 2 0}
+Output:
+0 0 1 2 2
+Explanation:
+0s 1s and 2s are segregated 
+into ascending order.
+*/
+//Initial template for Java
+
+import java.io.*;
+import java.util.*;
+
+
+ // } Driver Code Ends
+//User function template for Java
+
+class Solution
+{
+    public static void sort012(int a[], int n)
+    {
+        int[] d = new int[3];
+        for(int i =0;  i < n; i++){
+            d[a[i]] += 1;
+        }
+        int j = 0;
+        for(int i =0;i < n; ){
+            while(d[j] > 0){
+                a[i] = j;
+                d[j] = d[j]-1;
+                i++;
+            }
+            j++;
+        }
+    }
+}
+
+// { Driver Code Starts.
+
+class GFG {
+    
+    public static void main (String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim()); //Inputting the testcases
+        while(t-->0){
+            int n = Integer.parseInt(br.readLine().trim());
+            int arr[] = new int[n];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for(int i=0; i<n; i++){
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+            Solution ob=new Solution();
+            ob.sort012(arr, n);
+            StringBuffer str = new StringBuffer();
+            for(int i=0; i<n; i++){
+                str.append(arr[i]+" ");
+            }
+            System.out.println(str);
+        }
+    }
+}
+
+  // } Driver Code Ends
