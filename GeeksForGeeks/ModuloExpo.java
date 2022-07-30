732d45928d506171879d02bcac03300c71dec770
Mahmdudur Rahman Miraz <84429027+mrmiraz@users.noreply.github.com>
2022-07-30 10:03:23 +0600

    Modular Exponentiation for large numbers

---
+++ GeeksForGeeks/ModuloExpo.java
@@ -0,0 +1,59 @@
+/*
+Problem:
+Implement pow(x, n) % M.
+In other words, given x, n and M, find (xn) % M.
+ 
+
+Example 1:
+
+Input:
+x = 3, n = 2, m = 4
+Output:
+1
+Explanation:
+32 = 9. 9 % 4 = 1.
+*/
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            String s = br.readLine();
+            String[] S = s.split(" ");
+            long x = Long.parseLong(S[0]);
+            long n = Long.parseLong(S[1]);
+            long m = Long.parseLong(S[2]);
+            Solution ob = new Solution();
+            long ans = ob.PowMod(x, n, m);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public long PowMod(long x, long n, long m)
+    {
+        long res = 1;
+        while(n > 0){
+            if((n&1) == 1) res = (res*x)%m;
+            x = (x *x)%m;
+            n = n>>1;
+        }
+        return res;
+    }
+}
