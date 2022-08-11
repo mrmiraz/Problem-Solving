5abdd9c6a3d507d7dc9d1089012affa55ca63ee7
Mahmdudur Rahman Miraz <84429027+mrmiraz@users.noreply.github.com>
2022-08-11 14:00:11 +0600

    Common elements

---
+++ GeeksForGeeks/CommonElements.java
@@ -0,0 +1,107 @@
+/*
+Problem:
+Given three arrays sorted in increasing order. Find the elements that are common in all three arrays.
+Note: can you take care of the duplicates without using any additional Data Structure?
+
+Example 1:
+
+Input:
+n1 = 6; A = {1, 5, 10, 20, 40, 80}
+n2 = 5; B = {6, 7, 20, 80, 100}
+n3 = 8; C = {3, 4, 15, 20, 30, 70, 80, 120}
+Output: 20 80
+Explanation: 20 and 80 are the only
+common elements in A, B and C.
+*/
+
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+
+public class GFG
+{
+    public static void main(String args[]) 
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) 
+        {
+            int n1 = sc.nextInt();
+            int n2 = sc.nextInt();
+            int n3 = sc.nextInt();
+            
+            int A[] = new int[n1];
+            int B[] = new int[n2];
+            int C[] = new int[n3];
+            
+            for (int i = 0;i < n1;i++)
+            {
+                A[i] = sc.nextInt();
+            }
+            for (int i = 0;i < n2;i++)
+            {
+                B[i] = sc.nextInt();
+            }
+            for (int i = 0;i < n3;i++)
+            {
+                C[i] = sc.nextInt();
+            }
+            
+            Solution sol = new Solution();
+            ArrayList<Integer> res = sol.commonElements(A, B, C, n1, n2, n3);
+            if (res.size() == 0)
+            {
+                System.out.print(-1);
+            }
+            else 
+            {
+                for (int i = 0;i < res.size();i++)
+                {
+                    System.out.print(res.get(i) + " ");
+                }    
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    ArrayList<Integer> commonElements(int A[], int B[], int C[], int n1, int n2, int n3) 
+    {
+        ArrayList<Integer> ans = new ArrayList<>();
+        int i = 0, j = 0, k = 0;
+        while(i < n1 && j < n2){
+            if(A[i] == B[j]){
+                ans.add(A[i]);
+                i++;j++;
+            }
+            else if(A[i] < B[j])i++;
+            else j++;
+        }
+        
+        j = 0;
+        int len = ans.size();
+        ArrayList<Integer> newAns = new ArrayList<>();
+        while(j < len && k < n3){
+            if(ans.get(j) == C[k]){
+                if(!newAns.contains(C[k]))newAns.add(C[k]);
+                j++;k++;
+            }
+            else if(ans.get(j) < C[k]){
+                j++;
+            }
+            else{
+                k++;
+            }
+        }
+        return newAns;
+    }
+}
