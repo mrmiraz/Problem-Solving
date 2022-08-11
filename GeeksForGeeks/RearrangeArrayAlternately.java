916dd273dd3ffa11e8a15a91ce841cc0d221013e
Mahmdudur Rahman Miraz <84429027+mrmiraz@users.noreply.github.com>
2022-08-12 00:01:19 +0600

    Rearrange Array Alternately

---
+++ GeeksForGeeks/RearrangeArrayAlternately.java
@@ -0,0 +1,96 @@
+/*
+Problem:
+Given a sorted array of positive integers. Your task is to rearrange  the array elements alternatively i.e first element should be max value, second should be min value, third should be second max, fourth should be second min and so on.
+Note: Modify the original array itself.
+
+Example 1:
+
+Input:
+N = 6
+arr[] = {1,2,3,4,5,6}
+Output: 6 1 5 2 4 3
+Explanation: Max element = 6, min = 1, 
+second max = 5, second min = 2, and 
+so on... Modified array is : 6 1 5 2 4 3.
+
+*/
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+	public static void main (String[] args)throws IOException
+	{
+
+// 		Scanner in = new Scanner(System.in);
+        
+// 		int t = in.nextInt();
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        
+         //testcases
+        int t = Integer.parseInt(read.readLine());
+		
+		while(t-- >0)
+		{
+		    //size of array
+		    int n = Integer.parseInt(read.readLine());
+		    long[] arr= new long[n];
+		    
+		    String str[] = read.readLine().trim().split(" ");
+		    
+		    //adding elements to the array
+		    for(int i=0;i<n;i++)
+		        arr[i] = Long.parseLong(str[i]);
+		    
+		    // StringBuffer sb = new StringBuffer();
+		    
+		    Solution ob = new Solution();
+		    
+		    //calling rearrange() function
+		    ob.rearrange(arr, n);
+		    StringBuffer sb = new StringBuffer();
+		    
+		    //appending and printing the elements
+		    for(int i =0; i < n; i++)
+		     sb.append(arr[i] + " ");
+		    System.out.println(sb);
+		}
+	}
+}
+
+
+
+
+// } Driver Code Ends
+
+
+class Solution{
+    
+    // temp: input array
+    // n: size of array
+    //Function to rearrange  the array elements alternately.
+    public static void rearrange(long arr[], int n){
+        
+        int maxIdx = n-1, minIdx = 0;
+        long maxV = (arr[n-1]+1);
+        for(int i = 0;i < n; i++){
+            if((i&1) == 0){
+                arr[i] = arr[i]+(arr[maxIdx]%maxV)*maxV;
+                maxIdx--;
+            }
+            else{
+                arr[i] = arr[i]+(arr[minIdx]%maxV)*maxV;
+                minIdx++;
+            }
+        }
+        
+        for(int i =0; i < n; i++){
+            arr[i] = arr[i]/maxV;
+        }
+        
+    }
+    
+}
+
+
