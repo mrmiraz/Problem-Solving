10919f350b5eddd04fe8586b310cd0ce271d18cc
Mahmdudur Rahman Miraz <84429027+mrmiraz@users.noreply.github.com>
2022-08-18 19:46:16 +0600

    Triplet Sum in Array

---
+++ GeeksForGeeks/TripletSum.java
@@ -0,0 +1,70 @@
+/*
+Problem:
+Given an array arr of size n and an integer X. Find if there's a triplet in the array which sums up to the given integer X.
+
+
+Example 1:
+
+Input:
+n = 6, X = 13
+arr[] = [1 4 45 6 10 8]
+Output:
+1
+Explanation:
+The triplet {1, 4, 8} in 
+the array sums up to 13.
+*/
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Main
+{
+    public static void main (String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine().trim()); //Inputting the testcases
+		while(t-->0){
+		    String inputLine[] = br.readLine().trim().split(" ");
+		    int n = Integer.parseInt(inputLine[0]);
+		    int X = Integer.parseInt(inputLine[1]);
+		    int A[] = new int[n];
+		    inputLine = br.readLine().trim().split(" ");
+		    for(int i=0; i<n; i++){
+		        A[i] = Integer.parseInt(inputLine[i]);
+		    }
+		    Solution ob=new Solution();
+		    boolean ans = ob.find3Numbers(A, n, X);
+		    System.out.println(ans?1:0);
+		}
+	}
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    //Function to find if there exists a triplet in the 
+    //array A[] which sums up to X.
+    public static boolean find3Numbers(int A[], int n, int X) { 
+    
+        Arrays.sort(A);
+       for(int i = 0; i < n; i++){
+           
+           int left = i+1, right = n-1;
+           while(left < right){
+               int sum = A[left]+A[right]+A[i];
+               if(sum > X)right--;
+               else if(sum < X)left++;
+               else return true;
+           }
+       }
+       
+       return false;
+    
+    }
+}
