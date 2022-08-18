74bc284f91755dac887b1b52ac58f0dac62b7070
Mahmdudur Rahman Miraz <84429027+mrmiraz@users.noreply.github.com>
2022-08-18 11:34:36 +0600

    Array Subset of another array in cpp

---
+++ GeeksForGeeks/SubsetOfAnotherArray.cpp
@@ -0,0 +1,61 @@
+/*
+Problem:
+
+Given two arrays: a1[0..n-1] of size n and a2[0..m-1] of size m. Task is to check whether a2[] is a subset of a1[] or not. Both the arrays can be sorted or unsorted. 
+ 
+
+Example 1:
+
+Input:
+a1[] = {11, 1, 13, 21, 3, 7}
+a2[] = {11, 3, 7, 1}
+Output:
+Yes
+Explanation:
+a2[] is a subset of a1[]
+*/
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+string isSubset(int a1[], int a2[], int n, int m) ;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n, m;
+        cin >> n >> m;
+        int a1[n], a2[m];
+
+        for (int i = 0; i < n; i++) {
+            cin >> a1[i];
+        }
+        for (int i = 0; i < m; i++) {
+            cin >> a2[i];
+        }
+
+        cout << isSubset(a1, a2, n, m) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
+
+
+string isSubset(int a1[], int a2[], int n, int m) {
+    // vector<int> present(1000000);
+    vector<int> freq(1000001);
+    for(int i = 0;i < n; i++){
+        freq[a1[i]]++;
+    }
+    
+    for(int i = 0; i < m; i++){
+        if(freq[a2[i]] == 0)return "No";
+        freq[a2[i]]--;
+    }
+
+    return "Yes";
+}
