bfa22587ebc5e62800d3bc4ec7b57567091f310c
Mahmdudur Rahman Miraz <84429027+mrmiraz@users.noreply.github.com>
2022-08-18 11:34:42 +0600

    Array Subset of another array

---
+++ GeeksForGeeks/SubsetOfAnotherArray.java
@@ -0,0 +1,86 @@
+/*
+Problem:
+
+Given two arrays: a1[0..n-1] of size n and a2[0..m-1] of size m. Task is to check whether a2[] is a subset of a1[] or not. Both the arrays can be sorted or unsorted. 
+ 
+
+Example 1:
+
+Input:
+a1[] = {11, 1, 13, 21, 3, 7}
+a2[] = {11, 3, 7, 1}
+Output:
+Yes
+Explanation:
+a2[] is a subset of a1[]
+*/
+
+
+//{ Driver Code Starts
+//Initial Template for Java
+
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while(t-->0)
+        {
+            StringTokenizer stt = new StringTokenizer(br.readLine());
+            
+            long n = Long.parseLong(stt.nextToken());
+            long m = Long.parseLong(stt.nextToken());
+            long a1[] = new long[(int)(n)];
+            long a2[] = new long[(int)(m)];
+            
+            
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a1[i] = Long.parseLong(inputLine[i]);
+            }
+            String inputLine1[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < m; i++) {
+                a2[i] = Long.parseLong(inputLine1[i]);
+            }
+            
+            
+            Compute obj = new Compute();
+            System.out.println(obj.isSubset( a1, a2, n, m));
+            
+        }
+	}
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Compute {
+    public String isSubset( long a1[], long a2[], long n, long m) {
+        HashSet<Long> set =new HashSet<>();
+        HashMap<Long, Integer> map  = new HashMap<>();
+        for(int i = 0; i < n; i++){
+            if(!map.containsKey(a1[i])){
+                map.put(a1[i], 0);
+            }
+            map.put(a1[i], map.get(a1[i])+1);
+        }
+        for(int i = 0; i < m; i++){
+            if(!map.containsKey(a2[i])){
+                return "No";
+            }
+            if( map.get(a2[i]) == 0)return "No";
+            map.put(a2[i], map.get(a2[i])-1);
+        }
+        return "Yes";
+        
+    }
+}
