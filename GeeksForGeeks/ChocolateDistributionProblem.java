3dcfe62bc7e27c4a2a02945a36e4a6a4c5c3a5f9
Mahmdudur Rahman Miraz <84429027+mrmiraz@users.noreply.github.com>
2022-08-19 09:36:34 +0600

    Chocolate Distribution Problem

---
+++ GeeksForGeeks/ChocolateDistributionProblem.java
@@ -0,0 +1,69 @@
+/*
+Problem:
+Given an array A[ ] of positive integers of size N, where each value represents the number of chocolates in a packet. Each packet can have a variable number of chocolates. There are M students, the task is to distribute chocolate packets among M students such that :
+1. Each student gets exactly one packet.
+2. The difference between maximum number of chocolates given to a student and minimum number of chocolates given to a student is minimum.
+
+Example 1:
+
+Input:
+N = 8, M = 5
+A = {3, 4, 1, 9, 56, 7, 9, 12}
+Output: 6
+Explanation: The minimum difference between 
+maximum chocolates and minimum chocolates 
+is 9 - 3 = 6 by choosing following M packets :
+{3, 4, 9, 7, 9}.
+*/
+
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+class GfG
+{
+    public static void main (String[] args)
+    {
+        
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            int n = sc.nextInt();
+            ArrayList<Integer> arr = new ArrayList<>();
+            for(int i = 0;i<n;i++)
+                {
+                    int x = sc.nextInt();
+                    arr.add(x);
+                }
+            int m = sc.nextInt();
+            
+            Solution ob = new Solution();
+            
+    		System.out.println(ob.findMinDiff(arr,n,m));
+        }
+        
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public long findMinDiff (ArrayList<Integer> a, int n, int m)
+    {
+        Collections.sort(a);
+        long minDif = Integer.MAX_VALUE;
+        int left = 0, right = 0+m-1;
+        while(right < n){
+            minDif = Math.min(minDif, (long)(a.get(right)-a.get(left)));
+            right++;
+            left++;
+        }
+        return minDif;
+    }
+}
