6eb9ee5cca0cb4f1c481d82be4268c76a299d609
Mahmdudur Rahman Miraz <84429027+mrmiraz@users.noreply.github.com>
2022-08-20 14:41:00 +0600

    Smallest subarray with sum greater than x

---
+++ GeeksForGeeks/SubarraySumGreaterThanX.java
@@ -0,0 +1,76 @@
+/*
+Problem:
+Given an array of integers (A[])  and a number x, find the smallest subarray with sum greater than the given value.
+
+Note: The answer always exists. It is guaranteed that x doesn't exceed the summation of a[i] (from 1 to N).
+
+Example 1:
+
+Input:
+A[] = {1, 4, 45, 6, 0, 19}
+x  =  51
+Output: 3
+Explanation:
+Minimum length subarray is 
+{4, 45, 6}
+*/
+
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while(t-->0)
+        {
+            StringTokenizer stt = new StringTokenizer(br.readLine());
+            
+            int n = Integer.parseInt(stt.nextToken());
+            int m = Integer.parseInt(stt.nextToken());
+            // int n = Integer.parseInt(br.readLine().trim());
+            int a[] = new int[n];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a[i] = Integer.parseInt(inputLine[i]);
+            }
+            
+            Solution obj = new Solution();
+            System.out.println(obj.smallestSubWithSum(a, n, m));
+        }
+	}
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution {
+
+    public static int smallestSubWithSum(int a[], int n, int x) {
+        int ans = n, left = 0, right = 0;
+        int sum = 0;
+        while(right < n){
+            if(a[right] > x)return 1;
+            if((sum+a[right]) <= x){
+                sum+= a[right];
+                right++;
+            }
+            else{
+                ans = Math.min(ans, right-left+1);
+                sum -= a[left];
+                left++;
+            }
+        }
+        return ans;
+    }
+}
+
