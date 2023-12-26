9f9a540b16bf3697b6908bba50028edc810eb185
Mahmdudur Rahman Miraz <84429027+mrmiraz@users.noreply.github.com>
2023-12-26 15:11:53 +0600

    Reverse Vowels of a String

---
+++ Leetcode/345.java
@@ -0,0 +1,63 @@
+/**
+ Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
+
+ Consider the number of unique elements of nums to be k, to get accepted, you need to do the following things:
+
+ Change the array nums such that the first k elements of nums contain the unique elements in the order they were present in nums initially. The remaining elements of nums are not important as well as the size of nums.
+ Return k.
+ Custom Judge:
+
+ The judge will test your solution with the following code:
+
+ int[] nums = [...]; // Input array
+ int[] expectedNums = [...]; // The expected answer with correct length
+
+ int k = removeDuplicates(nums); // Calls your implementation
+
+ assert k == expectedNums.length;
+ for (int i = 0; i < k; i++) {
+ assert nums[i] == expectedNums[i];
+ }
+ If all assertions pass, then your solution will be accepted.
+
+
+
+ Example 1:
+
+ Input: nums = [1,1,2]
+ Output: 2, nums = [1,2,_]
+ Explanation: Your function should return k = 2, with the first two elements of nums being 1 and 2 respectively.
+ It does not matter what you leave beyond the returned k (hence they are underscores).
+ **/
+
+public class Hello {
+    public static String reverseVowels(String s) {
+        char[] stringArray = s.toCharArray();
+        boolean[] vowels = new boolean[128];
+        for(char c : "aeiouAEIOU".toCharArray()) vowels[c]=true;
+        int left = 0, right = s.length()-1;
+        while (left < right){
+            if (vowels[stringArray[left]] && vowels[stringArray[right]]){
+                char temp = stringArray[left];
+                stringArray[left] = stringArray[right];
+                stringArray[right] = temp;
+                left++;right--;
+            }
+            else if (vowels[stringArray[left]]){
+                right--;
+            }
+            else if (vowels[stringArray[right]]){
+                left++;
+            }
+            else {
+                left++;right--;
+            }
+        }
+        return String.valueOf(stringArray);
+    }
+
+    public static void main(String[] args) {
+        String a1 = "Euston saw I was not Sue.";
+        System.out.println(reverseVowels(a1));
+    }
+}
